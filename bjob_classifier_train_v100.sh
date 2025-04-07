8b0e45e2988e4a8985a1184cc57a059ebcfa1e0d
Joakim Dinh <dinhjoakim@gmail.com>
2025-04-07 13:09:02 +0200

    testing

--- bjob_classifier_train_v100.sh
+++ bjob_classifier_train_v100.sh
@@ -3,9 +3,10 @@
 ### -- specify queue --
 #BSUB -q gpuv100
 ### -- set the job Name -
-#BSUB -J DDPM_train_no_cfg
+#BSUB -J Classifier_training
 ### -- ask for number of cores (default: 1) --
-#BSUB -n 8
+#BSUB -n 16
+#BSUB -gpu "num=2:mode=exclusive_process" 
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
@@ -13,12 +14,12 @@
 # specify system resources
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=32GB]"
+#BSUB -R "select[gpu32gb]"
+#BSUB -R "select[sxm2]"
 ### -- NVlink required only for multigpu training [sxm2]
 ### BSUB -R "select[sxm2]"
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
-#BSUB -n 8
-#BSUB -gpu "num=2:mode=exclusive_process" 
 #BSUB -o batch_output/train_%J.out
 #BSUB -e batch_output/train_%J.err
 # -- end of LSF options --
